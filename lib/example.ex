c3122eeda7e3ca2c21b1770a8522eeb58675a0b0
Bhargava Prabu Reddy <prabu0reddy777@gmail.com>
2025-11-02 02:05:42 -0600

    Refactor membership and user structures for improved clarity and organization

--- lib/example.ex
+++ lib/example.ex
@@ -1,3 +1,11 @@
+defmodule Membership do
+  defstruct [:type, :price]
+end
+
+defmodule User do
+  defstruct [:name, :membership]
+end
+
 defmodule Example do
   @moduledoc """
   Documentation for `Example`.
@@ -46,36 +54,27 @@ defmodule Example do
     #     " " <> "minutes" <> " " <> Integer.to_string(remaining_seconds) <> " " <> "seconds"
     # )
 
-    # memberships = {:bronze, :silver, :gold}
-    # prices = {5, 10, 15}
-    # avg = Tuple.sum(prices) / tuple_size(prices)
+    memberships = {:bronze, :silver, :gold}
+    prices = {5, 10, 15}
+    avg = Tuple.sum(prices) / tuple_size(prices)
 
-    # IO.puts(
-    #   "Average price of #{elem(memberships, 0)}, #{elem(memberships, 1)}, #{elem(memberships, 2)} is #{avg}"
-    # )
+    IO.puts(
+      "Average price of #{elem(memberships, 0)}, #{elem(memberships, 1)}, #{elem(memberships, 2)} is #{avg}"
+    )
 
-    memberships = %{
-      platinum: :platinum,
-      gold: :gold,
-      silver: :silver,
-      none: :none
-    }
-
-    prices = %{
-      platinum: 100,
-      gold: 90,
-      silver: 80,
-      none: 0,
-    }
+    platinum_membership = %Membership{type: :platinum, price: 100}
+    gold_membership = %Membership{type: :gold, price: 90}
+    silver_membership = %Membership{type: :silver, price: 80}
+    # none_membership = %Membership{type: :none, price: 0}
 
     users = [
-      {"Moses", memberships.platinum},
-      {"Joshua", memberships.gold},
-      {"Caleb", memberships.silver}
+      %User{name: "Moses", membership: platinum_membership},
+      %User{name: "Joshua", membership: gold_membership},
+      %User{name: "Caleb", membership: silver_membership}
     ]
 
-    Enum.each(users, fn {name, membership} ->
-      IO.puts("#{name} has a #{membership} membership paying #{prices[membership]}$")
+    Enum.each(users, fn %User{name: name, membership: membership} ->
+      IO.puts("#{name} has a #{membership.type} membership paying #{membership.price}$")
     end)
   end
 end
